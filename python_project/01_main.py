ffe3ef71164ed4a29615a6ca5a06be04e65f893b
BigRFP <ryorico54@gmail.com>
2025-06-26 21:48:14 +0700

    test

---
+++ python_project/01_main.py
@@ -0,0 +1,7 @@
+from shapes_2d import luas_persegi, keliling_persegi
+
+s = float(input("Masukkan panjang sisi persegi: "))
+
+print("Luas persegi =", luas_persegi(s))
+print("Keliling persegi =", keliling_persegi(s))
+
---
+++ python_project/shapes_2d.py
@@ -0,0 +1,13 @@
+def luas_persegi(sisi):
+    """
+    Menghitung luas persegi.
+    Rumus: sisi × sisi
+    """
+    return sisi * sisi
+
+def keliling_persegi(sisi):
+    """
+    Menghitung keliling persegi.
+    Rumus: 4 × sisi
+    """
+    return 4 * sisi
